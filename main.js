67df2ff948f12ea303267e9c8f7dd403f0d14813
aaronfrost <aaronfrost@gmail.com>
2013-01-15 17:37:47 -0700

    a

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ var v = document.querySelector("#v"),
 	hl = document.querySelector('#hightlight'),
 	pixels;
 
+navigator.webkitGetUserMedia({video: true},function(stream){
+	v.src = URL.createObjectURL(stream);
+	v.play();
+});
+
+//Now let's add the video feed frames into the canvas
+// THIS IS HOW WE WILL ANALYZE THE FEED, IN THE CANVAS
+
