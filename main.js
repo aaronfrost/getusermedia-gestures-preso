4b585d55f55636b43e43f8563aa39236a664011b
aaronfrost <aaronfrost@gmail.com>
2013-01-15 17:49:14 -0700

    a

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var v = document.querySelector("#v"),
 	c = document.querySelector('#c'),
+	x = c.getContext('2d'),
 	hl = document.querySelector('#hightlight'),
 	pixels;
 
 navigator.webkitGetUserMedia({video:true},function(stream){
 	v.src = URL.createObjectURL(stream);
 	v.play();
+	setInterval(draw, 200);
 });
+
+
 
 //Let's put the video into the canvas now
